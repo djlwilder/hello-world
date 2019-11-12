cfa99bc9fd5dbe0a2c98a88568575940eea6e95a
David Wilder <dwilder@us.ibm.com>
2019-11-12 14:41:07 -0800

    fix aarch64

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -2,7 +2,7 @@
 
 
 [ $TRAVIS_ARCH = ppc64le ] && sudo apt-get install python-pip python-setuptools
-[ $TRAVIS_ARCH = aarm64 ] && sudo apt-get install python-pip python-setuptools
+[ $TRAVIS_ARCH = aarch64 ] && sudo apt-get install python-pip python-setuptools
 
 pip install --disable-pip-version-check --user six flake8 hacking
 pip install --user --upgrade docutils
