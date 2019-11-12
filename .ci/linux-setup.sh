d0fed0b5ca9cdaa92fdc78bc3e8ef4d00fd47bea
David Wilder <dwilder@us.ibm.com>
2019-11-12 14:02:03 -0800

    Fix syntax 2

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
 
-if [ $TRAVIS_ARCH == ppc64le ] || [ $TRAVIS_ARCH == aarm64 ]; then
+if [ $TRAVIS_ARCH = ppc64le ] || [ $TRAVIS_ARCH = aarm64 ]; then
 	apt-get install python-pip python-setuptools
 fi
