2f763c2d409e84c71f585ebcd531aa00c7ee3f1f
David Wilder <dwilder@us.ibm.com>
2019-11-12 14:34:33 -0800

    Install pip

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -xe
 
 
-if [ $TRAVIS_ARCH = ppc64le ] || [ $TRAVIS_ARCH = aarm64 ]; then
-	sudo apt-get install python-pip python-setuptools
-fi
+[ $TRAVIS_ARCH = ppc64le ] && sudo apt-get install python-pip python-setuptools
+[ $TRAVIS_ARCH = aarm64 ] && sudo apt-get install python-pip python-setuptools
+
+pip install --disable-pip-version-check --user six flake8 hacking
+pip install --user --upgrade docutils
